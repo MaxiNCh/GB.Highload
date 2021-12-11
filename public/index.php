a50ed64c9fc1929d64d9e1d7699a305674bbedaf
Maxim Zaboronkin <mvzaboronkin@gmail.com>
2021-12-11 13:41:14 +0300

    Установил monolog, xdebug.  Настроил на vscode, работет

--- public/index.php
+++ public/index.php
@@ -1,2 +1,28 @@
 <?php
+require_once('../vendor/autoload.php');
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$a1 = 1;
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('log/my.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Foo');
+$log->error('Bar');
+
+function deep_end($count)
+{
+  // добавляем 1 к параметру count
+  $count += 1;
+  if ($count < 48) {
+    deep_end($count);
+  }
+}
+deep_end(1);
+
+$log->warning('Memory ' . memory_get_usage());
+
 echo phpinfo();
